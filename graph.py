558ccd30b7f6c92da269c7267d77da1ba132d4af
Matthew Riley <mrriley@iastate.edu>
2021-08-24 12:56:20 -0700

    Add _str__ method to pring sudoku board.

--- graph.py
+++ graph.py
@@ -1,33 +1,59 @@
 class Graph:
 	def __init__(self, n: int):
+		self.size = n
 		self.vertices = [[0]*(n**2)]*(n**2)
 		self.edges = []
-		for i in range(n**4):
+		for i in range(self.size**4):
 			self.edges.append([])
 
-		for y in range(n**2):
-			for x in range(n**2):
+		for y in range(self.size**2):
+			for x in range(self.size**2):
 				edges = self.edges[((n**2)*y) + x]
-				x_region = int(x/n)
-				y_region = int(y/n)
+				x_region = int(x/self.size)
+				y_region = int(y/self.size)
 				
 				#This adds edges to cells in the same region.
-				for x_prime in range(0,n):
-					for y_prime in range(0,n):
-						if (x_region*n) + x_prime != x or (y_region*n) + y_prime != y:
-							edges.append(((x_region*n) + x_prime, (y_region*n) + y_prime))
+				for x_prime in range(0,self.size):
+					for y_prime in range(0,self.size):
+						if (x_region*self.size) + x_prime != x or (y_region*self.size) + y_prime != y:
+							edges.append(((x_region*self.size) + x_prime, (y_region*self.size) + y_prime))
 
 				#This adds edges to cells in the same column, outside the same region. 		
 				for row in range(n**2):
-					if int(row/n) != x_region:
+					if int(row/self.size) != x_region:
 						edges.append((row,y))
 
 				#This adds edges to the cells in the sme row, outside the same region.
 				for col in range(n**2):
-					if int(col/n) != y_region:
+					if int(col/self.size) != y_region:
 						edges.append((x,col))
 
-g = Graph(2)
-print(g.vertices)
-print(g.edges)
+	def __str__(self):
+		board = ''
+		horizontal_edge = ''
+		for i in range((self.size**2)+(self.size-1)+2):
+			horizontal_edge += '-'
+		board += horizontal_edge+'\n'
 
+		for y in range(self.size**2):
+			row = '|'
+			for x in range(self.size**2):
+				if self.vertices[y][x] == 0:
+					row +=' '
+				else:
+					row += str(self.vertices[y][x])
+				if (x+1)%self.size == 0:
+					row += '|'
+			
+			board += row+'\n'
+		
+			if (y+1)%self.size == 0:
+				board += horizontal_edge+'\n'
+			
+		return board
+
+	#todo: second constructor that reads from a file.
+	#todo: test __str__ after writing second constructor.
+
+g = Graph(3)
+print(g)
